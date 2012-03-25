8abde10d311e34b78dea992541e869a37fd55b0f
Tomás Lázaro <tomas@belfrygames.com>
2012-03-25 19:34:29 -0300

    Some reworking of Resources.

--- src/main/scala/com/belfrygames/starkengine/core/Resources.scala
+++ src/main/scala/com/belfrygames/starkengine/core/Resources.scala
@@ -6,20 +6,8 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import java.io.File
 
-trait Resources {
-  def initialize()
-  
-  def loadWithFile(file : File, width: Int = -1, height: Int = -1, x: Int = 0, y: Int = 0) : TextureRegion = {
-    loadWithFileHandle(new FileHandle(file), width, height, x, y)
-  }
-  
-  def load(name : String, width: Int = -1, height: Int = -1, x: Int = 0, y: Int = 0) : TextureRegion = {
-    loadWithFileHandle(Gdx.files.internal(name), width, height, x, y)
-  }
-  
+object Resources {
   def loadWithFileHandle(file : FileHandle, width: Int = -1, height: Int = -1, x: Int = 0, y: Int = 0) : TextureRegion = {
-    Gdx.app.log("Resources", "Loading: " + file)
-                
     val texture = new Texture(file)
     if (width < 0 || height < 0) {
       new TextureRegion(texture, x, y, texture.getWidth, texture.getHeight)
@@ -28,14 +16,12 @@ trait Resources {
     }
   }
   
-  def split(name: String, width: Int, height: Int, margin: Int, spacing: Int, flipX: Boolean, flipY: Boolean) : Array[Array[TextureRegion]] = {
+  def split(file : FileHandle, width: Int, height: Int, margin: Int, spacing: Int, flipX: Boolean, flipY: Boolean) : Array[Array[TextureRegion]] = {
     def indexToPos(x: Int, y: Int) : Tuple2[Int, Int] = {
         (margin + (width + spacing) * x, margin + (height + spacing) * y)
     }
     
-    Gdx.app.log("Resources", "Splitting: " + name)
-    
-    val texture = new Texture(Gdx.files.internal(name))
+    val texture = new Texture(file)
     
     val xSlices = texture.getWidth match {
       case n if n >= margin * 2 + width => 1 + (n - margin * 2 - width) / (width + spacing)
@@ -55,6 +41,22 @@ trait Resources {
     
     res
   }
+  
+  def split(name: String, width: Int, height: Int, margin: Int, spacing: Int, flipX: Boolean, flipY: Boolean) : Array[Array[TextureRegion]] = {
+    Resources.split(Gdx.files.internal(name), width, height, margin, spacing, flipX, flipY)
+  }
+  
+  def loadWithFile(file : File, width: Int = -1, height: Int = -1, x: Int = 0, y: Int = 0) : TextureRegion = {
+    Resources.loadWithFileHandle(new FileHandle(file), width, height, x, y)
+  }
+  
+  def load(name : String, width: Int = -1, height: Int = -1, x: Int = 0, y: Int = 0) : TextureRegion = {
+    Resources.loadWithFileHandle(Gdx.files.internal(name), width, height, x, y)
+  }
+}
+
+trait Resources {
+  def initialize()
   
   val map = collection.mutable.Map[String, TextureRegion]()
   def set(id: String, texture: TextureRegion) {
