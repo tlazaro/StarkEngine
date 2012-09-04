c6d00a70b895ac2952b3c8277bbacc9bf590c60a
Tomás Lázaro <tomas@belfrygames.com>
2012-09-04 14:55:41 -0300

    Made font not use integer rounding.

--- src/main/scala/com/belfrygames/starkengine/core/Graphic.scala
+++ src/main/scala/com/belfrygames/starkengine/core/Graphic.scala
@@ -16,11 +16,11 @@ object Graphic {
       case t => new Region(t)
     }
   }
-  
+
   def apply(texture: Texture) = {
     new Tex(texture)
   }
-  
+
   lazy val SQUARE: Graphic[_] = {
     import com.badlogic.gdx.graphics.{ Texture, Pixmap }
     import com.badlogic.gdx.graphics.g2d.Gdx2DPixmap
@@ -32,7 +32,7 @@ object Graphic {
   }
 }
 
-/** 
+/**
  * Since there is no cohesion among Libgdx primitives this class is needed.
  * This class should be extended to wrap visual primitives like a Texture, TextureRegion and BitmapFont
  */
@@ -40,7 +40,7 @@ trait Graphic[T <: AnyRef] {
   var color: Color = Color.WHITE.cpy // TODO: Move color to Node. Set color on spriteBatch. Have Text get Color from spritebatch
   var primitive: T
   def draw(spriteBatch: SpriteBatch, x: Float, y: Float, centerX: Float = 0, centerY: Float = 0, width: Float = width, height: Float = height, scaleX: Float = 1.0f, scaleY: Float = 1.0f, rotation: Float = 0)
-  
+
   def width: Float
   def height: Float
 }
@@ -52,7 +52,7 @@ class Region(override var primitive: TextureRegion) extends Graphic[TextureRegio
       spriteBatch.draw(primitive, x, y, centerX, centerY, width, height, scaleX, scaleY, rotation)
     }
   }
-  
+
   override def width: Float = if (primitive != null) primitive.getRegionWidth else -1
   override def height: Float = if (primitive != null) primitive.getRegionHeight else -1
 }
@@ -62,10 +62,10 @@ class TextureAtlasRegion(override var primitive: TextureAtlas.AtlasRegion) exten
     if (primitive != null) {
       spriteBatch.setColor(color)
       spriteBatch.draw(primitive, x + primitive.offsetX, y + primitive.offsetY, centerX, centerY,
-                       primitive.packedWidth, primitive.packedHeight, scaleX, scaleY, rotation)
+        primitive.packedWidth, primitive.packedHeight, scaleX, scaleY, rotation)
     }
   }
-  
+
   override def width: Float = if (primitive != null) primitive.originalWidth else -1
   override def height: Float = if (primitive != null) primitive.originalHeight else -1
 }
@@ -78,7 +78,7 @@ class Tex(override var primitive: Texture) extends Graphic[Texture] {
       spriteBatch.draw(region, x, y, centerX, centerY, width, height, scaleX, scaleY, rotation)
     }
   }
-  
+
   override def width: Float = if (primitive != null) primitive.getWidth else -1
   override def height: Float = if (primitive != null) primitive.getHeight else -1
 }
@@ -86,23 +86,26 @@ class Tex(override var primitive: Texture) extends Graphic[Texture] {
 object Text {
   val FONT_CHARACTERS =
     """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_¡!$%#@|\/¿?-+=()*&.;:,{}"´`'<>áéíóúüÁÉÍÓÚÜñÑ"""
-  
+
   def getText(pixelSize: Int, text: String): TrueTypeText = {
     new TrueTypeText(getFont(pixelSize), text)
   }
-  
+
   def getFont(pixelSize: Int): BitmapFont = {
-    TrueTypeFontFactory.createBitmapFont(
+    val font = TrueTypeFontFactory.createBitmapFont(
       Gdx.files.internal("com/belfrygames/starkengine/ubuntu.ttf"),
       FONT_CHARACTERS, 1024, 640, pixelSize, 1024, 640)
+
+    font.setUseIntegerPositions(false)
+    font
   }
 }
 
 class BitmapText(private var _primitive: BitmapFont, var text: String = "") extends Graphic[BitmapFont] {
   private var bounds: BitmapFont.TextBounds = null
-  
+
   primitive = _primitive
-  
+
   override def primitive_=(value: BitmapFont) = {
     _primitive = value
     if (_primitive != null) {
@@ -113,7 +116,7 @@ class BitmapText(private var _primitive: BitmapFont, var text: String = "") exte
     }
   }
   override def primitive = _primitive
-  
+
   override def draw(spriteBatch: SpriteBatch, x: Float, y: Float, centerX: Float, centerY: Float, width: Float, height: Float, scaleX: Float, scaleY: Float, rotation: Float) {
     if (_primitive != null) {
       _primitive.setScale(scaleX, scaleY)
@@ -123,13 +126,13 @@ class BitmapText(private var _primitive: BitmapFont, var text: String = "") exte
       _primitive.drawMultiLine(spriteBatch, text, textx, texty)
     }
   }
-  
+
   override def width: Float = {
     if (bounds != null) {
       (bounds.width / _primitive.getScaleX)
     } else -1
   }
-  
+
   override def height: Float = {
     if (_primitive != null) {
       ((_primitive.getLineHeight * text.lines.size) / _primitive.getScaleY)
@@ -139,9 +142,9 @@ class BitmapText(private var _primitive: BitmapFont, var text: String = "") exte
 
 class TrueTypeText(private var _primitive: BitmapFont, var text: String = "") extends Graphic[BitmapFont] {
   private var bounds: BitmapFont.TextBounds = null
-  
+
   primitive = _primitive
-  
+
   override def primitive_=(value: BitmapFont) = {
     _primitive = value
     if (_primitive != null) {
@@ -152,7 +155,7 @@ class TrueTypeText(private var _primitive: BitmapFont, var text: String = "") ex
     }
   }
   override def primitive = _primitive
-  
+
   override def draw(spriteBatch: SpriteBatch, x: Float, y: Float, centerX: Float, centerY: Float, width: Float, height: Float, scaleX: Float, scaleY: Float, rotation: Float) {
     if (_primitive != null) {
       _primitive.setScale(scaleX, scaleY)
@@ -162,13 +165,13 @@ class TrueTypeText(private var _primitive: BitmapFont, var text: String = "") ex
       _primitive.drawMultiLine(spriteBatch, text, textx, texty)
     }
   }
-  
+
   override def width: Float = {
     if (bounds != null) {
       (bounds.width / _primitive.getScaleX)
     } else -1
   }
-  
+
   override def height: Float = {
     if (_primitive != null) {
       ((_primitive.getLineHeight * text.lines.size) / _primitive.getScaleY)
